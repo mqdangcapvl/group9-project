14152c509b6480a966510e15e8b7a2bfd7a6c3c9
Jackie Trinh <trinhminhquan203@gmail.com>
2026-06-03 16:28:33 +0700

    change fonts style

--- interface/src/main.tsx
+++ interface/src/main.tsx
@@ -1,7 +1,18 @@
+import { createRoot } from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Poppins", sans-serif',
+  },
+});
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
+);
